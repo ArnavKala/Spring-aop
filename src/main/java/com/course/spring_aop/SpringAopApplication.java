60faaebdf6d57e62424d5e9a0b632f1f05a1453b
ArnavKala <akala8606@@gmail.com>
2026-04-06 12:16:31 +0530

    Adding Performance Traking(Timer) Aspect

--- src/main/java/com/course/spring_aop/SpringAopApplication.java
+++ src/main/java/com/course/spring_aop/SpringAopApplication.java
@@ -1,6 +1,7 @@
 package com.course.spring_aop;
 
 import com.course.spring_aop.aopexample.business.BusinessService1;
+import com.course.spring_aop.aopexample.business.BusinessService2;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -13,9 +14,11 @@ public class SpringAopApplication implements CommandLineRunner {
 	private Logger logger = LoggerFactory.getLogger(getClass());
 
 	private BusinessService1 businessService1;
+	private BusinessService2 businessService2;
 
-	public SpringAopApplication(BusinessService1 businessService1){
+	public SpringAopApplication(BusinessService1 businessService1, BusinessService2 businessService2){
 		this.businessService1=businessService1;
+		this.businessService2=businessService2;
 	}
 
 	public static void main(String[] args) {
@@ -24,6 +27,7 @@ public class SpringAopApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		logger.info("Value returned is {}", businessService1.calculateMax());
+		logger.info("BusinessService1 Value returned is {}", businessService1.calculateMax());
+		logger.info("BusinessService2 Value returned is {}", businessService2.calculateMin());
 	}
 }
--- src/main/java/com/course/spring_aop/aopexample/aspects/LoggingAspect.java
+++ src/main/java/com/course/spring_aop/aopexample/aspects/LoggingAspect.java
@@ -1,8 +1,7 @@
 package com.course.spring_aop.aopexample.aspects;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
@@ -16,9 +15,28 @@ public class LoggingAspect {
     //PointCut - When?
     //execution for all classes in a package execution(* PACKAGE.*.*(..))
     @Before("execution(* com.course.spring_aop.aopexample.*.*.*(..))")
-    public void logMethodCall(JoinPoint joinPoint){
+    public void logMethodCallBeforeExecution(JoinPoint joinPoint){
         //JoinPoint to get the details
         //Logic - What?
-        logger.info("Before Aspect - Method is called -{}", joinPoint);
+        logger.info("Before Aspect - {} is called with arguments -{}", joinPoint, joinPoint.getArgs());
+    }
+
+    @After("execution(* com.course.spring_aop.aopexample.*.*.*(..))")
+    public void logMethodCallAfterExecution(JoinPoint joinPoint){
+        logger.info("After Aspect - {} has executed", joinPoint);
+    }
+
+    @AfterThrowing(
+            pointcut = "execution(* com.course.spring_aop.aopexample.*.*.*(..))",
+            throwing ="exception")
+    public void logMethodCallAfterException(JoinPoint joinPoint, Exception exception){
+        logger.info("AfterThrowing Aspect - {} has thrown an exception ", joinPoint, exception);
+    }
+
+    @AfterReturning(
+            pointcut = "execution(* com.course.spring_aop.aopexample.*.*.*(..))",
+            returning = "resultValue")
+    public void logMethodCallAfterSuccessfulExecution(JoinPoint joinPoint, Object resultValue){
+        logger.info("After returning Aspect - {} has returned {}", joinPoint, resultValue);
     }
 }
